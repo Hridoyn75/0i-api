ada81b491a8d87c2fa62692aa82bca8bc09801fc
Nazmul Hridoy <hridoyn75@gmail.com>
2023-09-21 13:59:22 +0600

    stable 01

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const shortid = require('shortid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const links = [];
 
