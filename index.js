671a2128dbd796a1d75cd7f773a36fe88145701c
Nazmul Hridoy <hridoyn75@gmail.com>
2023-09-21 13:48:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const shortid = require('shortid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const links = [];
+
+app.use(express.json());
+
+// Create a route to shorten a URL using a GET request and query parameters
+app.get('/shorten', (req, res) => {
+  const longUrl = req.query.longUrl;
+
+  if(longUrl){
+    const shortUrl = shortid.generate();
+
+  const link = { longUrl,shortUrl };
+  links.push(link);
+  console.log(links)
+  res.json({ longUrl, "shortUrl": "https://0i.vercel.app/" + shortUrl });
+  }else{
+ res.json("Please send url with longUrl query");
+  }
+});
+
+// Redirect to the original URL when accessing the short URL
+app.get('/:shortUrl', (req, res) => {
+  const { shortUrl } = req.params;
+  const link = links.find((item) => item.shortUrl === shortUrl);
+
+  console.log(links)
+  if (link) {
+    res.redirect(link.longUrl);
+  } else {
+    res.status(404).json({ error: 'Link not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
